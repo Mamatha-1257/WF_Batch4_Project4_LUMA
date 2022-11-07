ea6a91439b6fcdc2aaee01644b8b80b5a7b9f951
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-11-07 18:46:35 +0530

    frontend user story 02

--- luma_frontend/src/pages/dashboard/Dashboard.jsx
+++ luma_frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Button } from "../../components/button/Button";
+import { useNavigate } from "react-router-dom";
+import "./Dashboard.css";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="dashboard">
+      <h1>Loan Management Application</h1>
+      <h2>User Dashboard</h2>
+      <div className="CTA">
+        <Button text="View Loans" onClick={() => navigate("/loans")}></Button>
+        <Button
+          text="Apply for Loan"
+          onClick={() => navigate("/loan-application")}
+        ></Button>
+        <Button
+          text="View Items Purchases"
+          onClick={() => navigate("/items")}
+        ></Button>
+      </div>
+    </div>
+  );
 };
--- luma_frontend/src/pages/index.js
+++ luma_frontend/src/pages/index.js
@@ -2,3 +2,5 @@ export { Loginpage } from "./loginpage/Loginpage";
 export { Homepage } from "./homepage/Homepage";
 export { LoanApplication } from "./loan_application/LoanApplication";
 export { Dashboard } from "./dashboard/Dashboard";
+export { Loans } from "./loans/Loans";
+export { Items } from "./items/Items";
